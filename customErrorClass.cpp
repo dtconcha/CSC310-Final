52b9d94ec2c0aaf261c271a18ca7c7b828d9277f
Vera-Alejandro <alejandro.vera-gonzalez@trojans.dsu.edu>
2020-04-18 12:32:26 -0500

    style adj

--- customErrorClass.cpp
+++ customErrorClass.cpp
@@ -1,21 +1,18 @@
 #include "customErrorClass.h"
 
-
-/**************************** PUBLIC: constructor ****************************/
+        //Public
 myException::myException(void)
 {
     this->p_ErrString = "ERROR: Program failed to execute correctly.";
     this->p_ErrCode = SYSTEM_FAILURE;
 }
 
-/**************************** PUBLIC: constructor ****************************/
 myException::myException(const char* p_ErrString, e_CODES p_ErrCode)
 {
     this->p_ErrString = p_ErrString;
     this->p_ErrCode = p_ErrCode;
 }
 
-/**************************** PUBLIC: constructor ****************************/
 myException::myException(const string &p_ErrString, e_CODES p_ErrCode)
 {
     this->p_ErrString = p_ErrString;
@@ -41,7 +38,8 @@ const e_CODES myException::increaseError(void)
     return retValue;
 }
 
-/**************************** PRIVATE: retrieveError ****************************/
+        //Private
+
 string myException::p_RetrieveError(void)
 {
     if(this->p_ErrCode == INFORMATIONAL )
